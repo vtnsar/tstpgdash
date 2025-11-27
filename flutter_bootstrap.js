ee1287b855d3581366f186b82d3c1bc23fa601ca
Koli <kolinior@gmail.com>
2025-11-28 02:06:00 +1200

    UI update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2547832824"
+    serviceWorkerVersion: "1917980518"
   }
 });
